2b80aa3e173747ddf7f3a56ab3f24f3999208b33
BTK Development <btkwebmaster@gmail.com>
2018-08-12 15:28:09 -0400

    Create Environment example file

---
+++ src/renderer/common/Environment.js.example.js
@@ -0,0 +1,20 @@
+// Token Information
+const tokenName 				= 'Your Token Name';
+const tokenTicker 			= 'TKN';
+const contractAddress 	= 'Your token contract address';
+const abi               = JSON.parse('[{Your token ABI}]');
+
+// API Key Information
+const infuraApiKey			= '';
+const ethplorerApiKey		= '';
+const coinlibApiKey			= '';
+
+module.exports = {
+	tokenName,
+	tokenTicker,
+	contractAddress,
+  abi,
+	infuraApiKey,
+	ethplorerApiKey,
+	coinlibApiKey
+}
